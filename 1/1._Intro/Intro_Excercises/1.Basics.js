20ee57110d53375b49494a1dd12799fa9cd8f151
MarkusDrejer <mabd412b@stud.kea.dk>
2020-02-06 09:51:41 +0100

    Added the first Excercises

---
+++ 1/1._Intro/Intro_Excercises/1.Basics.js
@@ -0,0 +1,13 @@
+var firstName = "Markus";
+var lastName = "Drejer";
+
+console.log("My first name is", firstName, "and my last name is", lastName);
+
+//console.log(`My first name is ${firstname}`)
+
+var year = "2017";
+var number = 1;
+
+console.log(parseInt(year) + number);
+
+console.log(Number(year) + number);
