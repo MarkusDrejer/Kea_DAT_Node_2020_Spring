a00df48ff3b401f899f320bcdf44ef263490ae23
MarkusDrejer <mabd412b@stud.kea.dk>
2020-02-20 13:55:26 +0100

    Added jQuery and more paths

--- 1/2._First_API/app.js
+++ 1/2._First_API/app.js
@@ -1,4 +1,6 @@
+//require is import
 const express = require("express");
+const request = require('request');
 const app = express();
 
 //One liner:
@@ -16,7 +18,7 @@ app.get("/aboutMe", (req, res) => {
         profession: "Computers"
     }
     res.send(me)
-})
+});
 
 app.listen(3000, error => {
     if (error) {
@@ -24,7 +26,6 @@ app.listen(3000, error => {
     }
     console.log("Server is running on port 3000");
 });
-
 
 app.get("/aboutThisWebsite", (req, res) => {
     const thisWebSite = {
@@ -62,4 +63,63 @@ app.get("/allOfUs", (req, res) => {
         someoneElse
     }
     res.send(us)
-})
+});
+
+app.get("/time", (req, res) => {
+    const date = new Date();
+    return res.send({
+        date: date.getDate(), 
+        month: convertMonth(date.getMonth()),
+        year: date.getFullYear(),
+        time: date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds(),
+        day: convertDay(date.getDay()),
+        anotherDay: date.toLocaleString("en-us", {weekday: "long"})
+    });
+});
+
+const convertDay = (dayIndex) => {
+    const dayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return dayArray[dayIndex];
+};
+
+const convertMonth = (monthIndex) => {
+    const monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    return monthArray[monthIndex];
+};
+
+app.get("/user/:id", (req, res) => {
+    console.log(req.params);
+    return res.send({
+        id: req.params.id
+    });
+});
+
+app.get("/search", (req, res) => {
+
+    return res.send({
+        query: req.query.id
+    });
+});
+
+app.get("/google", (req, res) => {
+    request('http://www.google.com', (error, response, body) => {
+        console.error('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        return res.send(body);
+    });
+});
+
+app.get("/documentation", (req, res) => {
+    //return res.redirect("/documentationtwo");
+    return res.sendFile(__dirname + "/public/documentation.html");
+});
+
+//Universal weird route to point to any file in the system via a path variable
+//app.get("/:name", (req, res) => {
+//    return res.sendFile(__dirname + "/public/" + req.params.name + ".html");
+//});
+
+app.get("/documentationtwo", (req, res) => {
+    return res.sendFile(__dirname + "/public/documentationtwo.html");
+});
---
+++ 1/3._jQuery/1._intro_to_jQuery/index.js
@@ -0,0 +1,5 @@
+$("#teleport-btn").click(() => {
+    const temp = $(".input-right").val();
+    $(".input-right").val($(".input-left").val());
+    $(".input-left").val(temp);
+});
