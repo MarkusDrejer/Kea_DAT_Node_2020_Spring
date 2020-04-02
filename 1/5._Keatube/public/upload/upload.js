c6cf7b297b1034708e9ce1e4191fad330181ac81
mabd <mabd@itu.dk>
2020-04-02 12:08:59 +0200

    working system

--- 1/5._Keatube/public/upload/upload.js
+++ 1/5._Keatube/public/upload/upload.js
@@ -1,3 +1,39 @@
+let fileValid = false;
+
 function validateForm() {
-    return true;
+
+    const title = document.forms.videoupload.title.value;
+    const description = document.forms.videoupload.description.value;
+    const tags = document.forms.videoupload.tags.value;
+    const category = document.forms.videoupload.category.value;
+
+    if (title.length < 8 || title.length > 64) {
+        return false;
+    }
+
+    if (description > 2048) {
+        return false;
+    }
+
+    return true && fileValid;
+}
+
+function handleFileUpload(files) {
+    const file = files[0];
+    const fileSize = file.size;
+    const mimeArray = file.type.split("/");
+    const fileType = mimeArray[0];
+
+    if (fileType !== "video") {
+        fileValid = false;
+        return;
+    }
+
+    twoGBFileLimit = 2147483548;
+
+    if (fileSize > twoGBFileLimit) {
+        fileValid = false;
+        return;
+    }
+    fileValid = true;
 }
