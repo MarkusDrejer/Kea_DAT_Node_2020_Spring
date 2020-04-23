30b3c96d02dcc773248177d5139c776d838b4dee
mabd <mabd@itu.dk>
2020-04-23 11:59:48 +0200

    Added credentials and scripts to add to database schema

---
+++ 1/7._Auth/migrations/20200423100208_initial_schema.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('users', (table) => {
+            table.increments("id");
+            table.string("username").unique().notNullable();
+            table.string("password").notNullable();
+            table.integer("age");
+            table.timestamps();
+            table.timestamp("updated_at");
+            table.timestamp("created_at").defaultTo(knex.fn.now());
+            // table.timestamp("created_at").defaultTo(knex.fn.now());
+
+        });
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists("users");
+};
